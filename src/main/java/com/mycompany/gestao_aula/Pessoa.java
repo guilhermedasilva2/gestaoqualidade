094ca87a36586b2d347ff0d6b21938a89ed3bc50
guilh <g19062003guilhermedasilva2003@gmail.com>
2025-06-18 08:39:33 -0300

    adiçao da clase pessoa

---
+++ src/main/java/com/mycompany/gestao_aula/Pessoa.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.mycompany.gestao_aula;
+
+/**
+ *
+ * @author guilh
+ */
+public class Pessoa {
+
+    private String nome;
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
