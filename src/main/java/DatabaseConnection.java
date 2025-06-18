28378b0e1765e6e20df1cf6c61e520d6f508988c
guilh <g19062003guilhermedasilva2003@gmail.com>
2025-06-18 07:58:37 -0300

    adiçao de databaseconnection

---
+++ src/main/java/DatabaseConnection.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3307/cadastro";
+    private static final String USER = "usuario";
+    private static final String PASSWORD = "senha";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Garante que o driver esteja carregado
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao conectar ao banco de dados", e);
+        }
+    }
+}
